6fcfd6a2ceb634fceec8b75a3a05f5badaed6e3a
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-03-31 20:04:01 +0100

    task/PW-23 - Initially setup gulpfile and css minification

---
+++ Personal Website/gulpfile.js
@@ -0,0 +1,16 @@
+﻿let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+
+gulp.task('minify-css', () => {
+    return gulp.src('./Website/styles/*.css')
+        .pipe(cleanCSS())
+        .pipe(concat('site.min.css'))
+        .pipe(gulp.dest('./Website/styles'));
+});
+
+/*gulp.task('styles', function () {
+    gulp.watch('./Website/styles/*.css', function (evt) {
+        gulp.task('minify-css');
+    });
+});*/
