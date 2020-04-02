c36cdc69509f04b25e1682e44ba2409cb4520562
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-04-02 21:37:06 +0100

    task/PW-28 - Improved styling, gulpfile and publish exclusions

--- Personal Website/gulpfile.js
+++ Personal Website/gulpfile.js
@@ -3,6 +3,7 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 
 gulp.task('minify-css', () => {
     return gulp.src('./Website/styles/*.css')
@@ -11,8 +12,8 @@ gulp.task('minify-css', () => {
         .pipe(gulp.dest('./Website/styles'));
 });
 
-/*gulp.task('styles', function () {
+gulp.task('styles', function () {
     gulp.watch('./Website/styles/*.css', function (evt) {
         gulp.task('minify-css');
     });
-});*/
+});
