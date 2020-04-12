b4ddda984d1a143e7a259cba2a6437271685c664
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-04-12 16:01:00 +0100

    task/PW-133 - Got fading content working for experience and inner scrollbar

---
+++ Personal Website/Website/js/ScrollFade.js
@@ -0,0 +1,41 @@
+﻿function isVisible(element) {
+    let contentBox = document.getElementsByClassName('experienceTable')[0].getBoundingClientRect();
+    let elementBox = element.getBoundingClientRect();
+    const distanceFromBottom = 80;
+
+    if (elementBox.top <= (contentBox.bottom - distanceFromBottom) && elementBox.top >= contentBox.top)
+        return true;
+    else
+        return false;
+}
+
+function scanContent() {
+    let elementList = document.querySelectorAll('.experienceTable-item');
+    elementList.forEach(function (element) {
+        if (isVisible(element)) {
+            element.classList.remove('hidden');
+            element.classList.add('visible');
+        }
+        else
+        {
+            element.classList.remove('visible');
+            element.classList.add('hidden');
+        }
+    });
+}
+
+$(document).ready(function () {  
+    document.getElementsByClassName("experienceTable")[0].onscroll = scanContent;
+
+    setTimeout(scanContent, 60);
+});
+
+function throttle(fn, wait) {
+    var time = Date.now();
+    return function () {
+        if ((time + wait - Date.now()) < 0) {
+            fn();
+            time = Date.now();
+        }
+    }
+}
